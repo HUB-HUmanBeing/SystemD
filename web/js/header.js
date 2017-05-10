a24f3c3aee315b020892872e8c487d777c4dd024
robinBanquo <caramaschi@hotmail.com>
2017-05-10 22:55:03 +0200

    préparation du travail Javascript sur le menu-header

---
+++ web/js/header.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    //animation du bouton rechercher
+    $('.header-nav #search-btn').click(function () {
+        alert("coucou!")
+        //déplacement des autres boutons vers la droite et la gauche
+
+        //apparition du champs de recherche et décalage du bouton rechercher sur la droite
+
+        //focus du clavier dans le champs de recherche
+
+        //activation du bouton rechercher avec deux comportements :
+        // referme le bouton menu si rien n'est rentré
+
+        //lance la recherche si quelquechose est entré en input
+    });
+});
