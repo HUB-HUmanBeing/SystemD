e02ade8d1c1402a2cc410c5f926705a5b5266a31
robinBanquo <caramaschi@hotmail.com>
2017-05-12 00:16:17 +0200

    animation du menu supérieur

--- web/js/header.js
+++ web/js/header.js
@@ -1,16 +1,34 @@
+//fait par banquo
+
+//TODO :
+// fluidifier l'animation fermante
+// gerer le cadre rouge
+// permetre la fermeture lorsqu'on clique ailleur sur la page
+
+
 $(document).ready(function () {
-    //animation du bouton rechercher
-    $('.header-nav #search-btn').click(function () {
-        alert("coucou!")
+    //animation affichant et refermant le bouton rechercher
+    var searchbar = false;
+
+    $('.header-nav #search').click(function showSearchBar() {
+        //on commence par fixer la taille du bouton recherche sur sa taille initiale.
+        var widthimg = $('.header-nav img').width();
+        $('.header-nav img').css("width", widthimg);
+
         //déplacement des autres boutons vers la droite et la gauche
+        $('.header-nav #home-btn').toggle("slide", {direction: 'right'}, 500);
+        $('.header-nav .left-menu').toggle("slide", 500, function () { //quant c'est fini,
+            //apparition du champs de recherche
+            $('.header-nav .search-field').toggle("slide", 200);
+            //focus du clavier dans le champs de recherche
+            $('.header-nav .search-field').focus();
+        });
 
-        //apparition du champs de recherche et décalage du bouton rechercher sur la droite
-
-        //focus du clavier dans le champs de recherche
-
-        //activation du bouton rechercher avec deux comportements :
-        // referme le bouton menu si rien n'est rentré
-
-        //lance la recherche si quelquechose est entré en input
+        //on annonce au reste du script que la searchbar est visible
+        searchbar = true ;
     });
+//todo : à debugger
+//     if ( searchbar ) {
+//         $('main').click( showSearchBar() );
+//     }
 });
