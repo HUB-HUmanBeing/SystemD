1c2190ed500abd8eb0e00c9983e53f8384391cf1
robinBanquo <caramaschi@hotmail.com>
2017-05-12 12:15:08 +0200

    animation du menu supérieur terminé

--- web/js/header.js
+++ web/js/header.js
@@ -1,34 +1,53 @@
 //fait par banquo
 
-//TODO :
-// fluidifier l'animation fermante
-// gerer le cadre rouge
-// permetre la fermeture lorsqu'on clique ailleur sur la page
-
+var searchbar = false;     // = la searchbar est elle active?
+function closesearch() {
+    $('.header-nav .search-field').toggle("slide", 200, function () { //quant c'est fini,
+        //on fait réaparaitre les autres boutons
+        $('.header-nav .left-menu').toggle("slide", 500);
+        setTimeout(function () {
+            $('.header-nav #home-btn').toggle("slide", {direction: 'right'}, 500);
+        }, 200);
+        //on annonce au reste du script que la searchbar est cachée
+        searchbar = false;
+    });
+}
 
 $(document).ready(function () {
     //animation affichant et refermant le bouton rechercher
-    var searchbar = false;
 
-    $('.header-nav #search').click(function showSearchBar() {
-        //on commence par fixer la taille du bouton recherche sur sa taille initiale.
-        var widthimg = $('.header-nav img').width();
-        $('.header-nav img').css("width", widthimg);
 
-        //déplacement des autres boutons vers la droite et la gauche
-        $('.header-nav #home-btn').toggle("slide", {direction: 'right'}, 500);
-        $('.header-nav .left-menu').toggle("slide", 500, function () { //quant c'est fini,
-            //apparition du champs de recherche
-            $('.header-nav .search-field').toggle("slide", 200);
-            //focus du clavier dans le champs de recherche
-            $('.header-nav .search-field').focus();
-        });
+    $('.header-nav #open-search').click(function () {
+        if(!searchbar) {
+            //on commence par fixer la taille du bouton recherche sur sa taille initiale.
+            var widthimg = $('.header-nav img').width();
+            $('.header-nav img').css("width", widthimg);
 
-        //on annonce au reste du script que la searchbar est visible
-        searchbar = true ;
+            //déplacement des autres boutons vers la droite et la gauche
+            $('.header-nav #home-btn').toggle("slide", {direction: 'right'}, 500);
+            $('.header-nav .left-menu').toggle("slide", 500, function () { //quant c'est fini,
+                //apparition du champs de recherche
+                $('.header-nav .search-field').toggle("slide", 200);
+                //focus du clavier dans le champs de recherche
+                $('.header-nav .search-field').focus();
+                //on annonce au reste du script que la searchbar est visible
+                searchbar = true ;
+
+            });
+        }
+        else{
+            if($('.search-field').val() !== ""){  //si ya quelquechose dedans on envoie
+                $('#search').submit();
+            }
+            else {
+                closesearch();
+            }
+        }
     });
-//todo : à debugger
-//     if ( searchbar ) {
-//         $('main').click( showSearchBar() );
-//     }
+    $('main').click(function () {
+        if(searchbar){
+            closesearch();
+        }
+
+    })
 });
