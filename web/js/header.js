ad39ed2d854e93efe4ae180322479749332fc363
robinBanquo <caramaschi@hotmail.com>
2017-06-18 23:31:55 +0200

    essai de navabar au comportement sympatique

--- web/js/header.js
+++ web/js/header.js
@@ -1,2 +1,7 @@
 //fait par banquo
+// grab an element
+var myElement = document.querySelector(".general-header");
+// construct an instance of Headroom, passing the element
+var headroom = new Headroom(myElement);
+headroom.init();
 
