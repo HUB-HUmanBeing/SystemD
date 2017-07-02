bab1df3741275b13b5b5d9d1d23d98bf8a1d25d6
robinBanquo <caramaschi@hotmail.com>
2017-07-02 23:34:25 +0200

    vue pour edit des infos projet

--- src/ProjectBundle/Controller/ProjectController.php
+++ src/ProjectBundle/Controller/ProjectController.php
@@ -6,28 +6,39 @@ use ProjectBundle\Entity\Project;
 use ProjectBundle\Form\ProjectType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 
-
+/**
+ * Class ProjectController
+ * @package ProjectBundle\Controller
+ */
 class ProjectController extends Controller
 {
-    /**
+    /*
      * action permetant de récuperer les infos nécessaires a l'affichage de la page "vitrine" de chaques
-     * @param $id
-     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function MainPageAction($id)
+    public function MainPageAction($id , Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $project = $em->getRepository('ProjectBundle:Project')->find($id);
         return $this->render('@Project/Project/mainpage.html.twig',
             array(
                 'project'=> $project,
+                "initialRequest" =>$request,
                 'UserRoleInProject' => $this->get('project.ProjectAuth')->getUserRole($project)
             )
         );
     }
     // renvoie la page des membres d'un projet  et le panneau d'envoi d'invitation d'un utilisateur
+
+    /**
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function membersAction($id, Request $request)
     {
 
@@ -43,6 +54,11 @@ class ProjectController extends Controller
             'UserRoleInProject' => $this->get('project.ProjectAuth')->getUserRole($project)
         ));
     }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function newProjectAction(Request $request){
         //on génere un nouveau projet
         $Project = new Project();
@@ -66,16 +82,27 @@ class ProjectController extends Controller
         return $this->render('ProjectBundle:Project:newProject.html.twig', array(
             'form' => $form->createView(),
             'path' => $this->generateUrl('new_project'),
+
         ));
     }
 
+    /**
+     * @param $project_id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function editInfoAction($project_id , Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $Project = $em->getRepository('ProjectBundle:Project')->find($project_id);
-        $form = $this->get('form.factory')->create(ProjectType::class, $Project);
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $Project);
+        $formBuilder->add('description',   TextareaType::class)
+                    ->add('save',  SubmitType::class);
+        $form = $formBuilder->getForm();
         //si la requete est de type post et si le formulaire est valide (crsf etc)
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            //on peut alors vérifie que c'est bien un'admin du projet
+            $this->get('project.ProjectAuth')->adminAuthCheck($Project);
             //on persiste le tout
             $em->persist($Project);
             $em->flush();
@@ -84,8 +111,9 @@ class ProjectController extends Controller
             //quant c'est fait, on renvoie vers la page d'aceuil du projet
             return $this->redirectToRoute('project_mainpage' , array('id' => $project_id));
         }
-        return $this->render('ProjectBundle:Project:newProject.html.twig', array(
+        return $this->render('ProjectBundle:Project:editInfo.html.twig', array(
             'form' => $form->createView(),
+            'project_name' =>$Project->getName(),
             'path' => $this->generateUrl('edit_project_info', array('project_id' => $project_id)),
         ));
     }
