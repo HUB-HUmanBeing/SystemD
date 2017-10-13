7856fa27833e1a33916fcf31fcc50a24d4328f4a
robinBanquo <caramaschi@hotmail.com>
2017-10-13 12:32:55 +0200

    debut de confection du menu contextuel

---
+++ client/templates/includes/contextualMenu/contextualMenu.js
@@ -0,0 +1,26 @@
+Template.contextualMenu.helpers({
+    //add you helpers here
+    contextualTitle : function () {
+arborescenceStructure().forEach(function (item) {
+    
+})
+    }
+});
+
+Template.contextualMenu.events({
+    //add your events here
+});
+
+Template.contextualMenu.onCreated(function () {
+    //add your statement here
+
+});
+
+Template.contextualMenu.onRendered(function () {
+    //add your statement here
+});
+
+Template.contextualMenu.onDestroyed(function () {
+    //add your statement here
+});
+
