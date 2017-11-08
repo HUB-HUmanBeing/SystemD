b65c248fcf1f7b02f18ff44f9a6f8bce6382095e
J3m5 <alphalight@protonmail.com>
2017-11-08 17:12:12 +0100

    Sign in test finished

--- client/templates/User/userSelfProfile/userSelfProfile.js
+++ client/templates/User/userSelfProfile/userSelfProfile.js
@@ -1,55 +1,57 @@
-import User from '/imports/classes/User'
+import User from '/imports/classes/User';
 
 Template.userSelfProfile.helpers({
-    completed : function () {
-        return User.findOne(Meteor.userId()).completed();
-    },
-    isDeletable : function () {
-        let user = User.findOne({_id : Meteor.userId()})
-        return user.isDeletable()
-    }
+  completed () {
+    return User.findOne(Meteor.userId()).completed();
+  },
+  isDeletable () {
+    let user = User.findOne({ _id: Meteor.userId() });
+    return user.isDeletable();
+  },
 });
 
 Template.userSelfProfile.events({
-    //aaction de suppression definitive d'un compte utilisateur
-    'click [deleteUserAccount]': function () {
-        console.log("ok")
-        //on récupere l'utilisateur courant
-        let currentUser = User.findOne({_id: Meteor.userId()})
-        $('.modal').modal('close');
-        //on redirige vers la page des projets utilisateurs
-        Router.go('home')
-        //on appele la methode de suppression
-        currentUser.callMethod(
-            'deleteUserAccount',
-            (error) => {
-                //si ca marche pas, on renvoie l'erreur par toast
-                if (error) {
-                    Materialize.toast("une erreur s'est produite", 4000, 'red')
-                } else {
-                    //sinon, on toast le succes,
-                    Materialize.toast("Votre compte utilisateur à été définitivement supprimé", 6000, 'green')
-                    //on ferme la modal
-
-                }
-            })
-    }
+  // aaction de suppression definitive d'un compte utilisateur
+  'click [deleteUserAccount]': function () {
+    console.log('ok');
+    // on récupere l'utilisateur courant
+    const currentUser = User.findOne({ _id: Meteor.userId() });
+    $('.modal').modal('close');
+    // on redirige vers la page des projets utilisateurs
+    Router.go('home');
+    // on appele la methode de suppression
+    currentUser.callMethod(
+      'deleteUserAccount',
+      (error) => {
+        // si ca marche pas, on renvoie l'erreur par toast
+        if (error) {
+          Materialize.toast("une erreur s'est produite", 4000, 'red');
+        } else {
+          // sinon, on toast le succes,
+          Materialize.toast('Votre compte utilisateur à été définitivement supprimé', 6000, 'green');
+          // on ferme la modal
+        }
+      },
+    );
+  },
 });
 
-Template.userSelfProfile.onCreated(function () {
+Template.userSelfProfile.onCreated(() => {
 
 });
 
 //
-Template.userSelfProfile.onRendered(function () {
-    $('.modal').modal();
-    $('.collapsible').collapsible();
-    $('.swipable').tabs(
-        // { 'swipeable': true }
-    );
+Template.userSelfProfile.onRendered(() => {
+  $('.modal').modal();
+  $('.collapsible').collapsible();
+  $('.swipable').tabs(
+    // { 'swipeable': true }
+  );
+  console.log(Template.instance());
+  console.log(Template.currentData());
 });
 
-Template.userSelfProfile.onDestroyed(function () {
-    //add your statement here
+Template.userSelfProfile.onDestroyed(() => {
+  // add your statement here
 });
 
