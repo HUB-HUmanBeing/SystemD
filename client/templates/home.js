fbb86cb5d9e6f98fed093ebbe9387a0732edd10d
robinBanquo <caramaschi@hotmail.com>
2017-11-12 20:03:22 +0100

    createur de fixtures pour héroku

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,26 +1,26 @@
 Template.home.helpers({
-  // add you helpers here
-  isDevelopment() {
-    return Meteor.isDevelopment;
-  },
+    // add you helpers here
+    isDevelopment() {
+        return Meteor.isDevelopment;
+    },
     isRobin() {
-    return Meteor.user().username === "robin"
+        return Meteor.user().username === "robin"
     }
 });
 
 Template.home.events({
-  // add your events here
+    // add your events here
 });
 
 Template.home.onCreated(() => {
-  // add your statement here
+    // add your statement here
 });
 
 Template.home.onRendered(() => {
-  // add your statement here
+    // add your statement here
 });
 
 Template.home.onDestroyed(() => {
-  // add your statement here
+    // add your statement here
 });
 
