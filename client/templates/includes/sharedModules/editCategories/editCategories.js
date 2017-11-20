ab50210175eee211712ae6087c890e86aa2dc6a7
robinBanquo <caramaschi@hotmail.com>
2017-11-20 18:55:18 +0100

    ajout du package-lock.json et début des commentaires catégories

--- client/templates/includes/sharedModules/editCategories/editCategories.js
+++ client/templates/includes/sharedModules/editCategories/editCategories.js
@@ -2,54 +2,68 @@ import User from '/imports/classes/User';
 import Project from '/imports/classes/Project';
 import Sortable from 'sortablejs';
 
+//documentation pour sortable : https://github.com/RubaXa/Sortable
+
 Template.editCategories.helpers({
-    //add you helpers here
+    //true si le template est en mode édition
     isEditing: function () {
         return Template.instance().isEditing.get()
     },
+    //liste des catégories de l'utilisateur(initialement)
     categories: function () {
         return Template.instance().categories
     },
+    //liste des catégories non-choisies par l'utilisateur (initialement)
     unchosenCategories: function () {
         return Template.instance().unchosenCategories
     },
+    //true si il y a eu des opérations faites par l'utilisateur
     isEdited: function () {
         return Template.instance().isEdited.get()
     },
+    //nom a donner suivant l'endroit ou le template est appelé
     categoriesLabel: function () {
         return Template.instance().isProject ? "types d'activités" : "centres d'interets"
     },
-    categoriesLabelColor : function () {
+    //couleur du label suivant l'endroit ou le template est appelé
+    categoriesLabelColor: function () {
         return Template.instance().isProject ? "orange" : "green"
     }
 
 });
 
 Template.editCategories.events({
+    //lorsque le bouton editer est cliqué
     'click [editCategoriesBtn]': function (event, instance) {
+        //on switch le template sur on ou of suivant si on est deja  en mode édition
         instance.isEditing.set(!instance.isEditing.get())
         resetTooltips()
         Meteor.setTimeout(() => {
+            //on récupere la div des documents non choisis
             let unchosenCategories = document.getElementById("unchosenCategories")
+            //on active le tri par glisser deposer
             this.unchosen = Sortable.create(unchosenCategories, {
                 group: {
                     name: "unchosenCategories",  // or { name: "...",
-                    put: ['chosenCategories']
+                    put: ['chosenCategories'] //on peut faire rentrer dedans les catégories déja choisies
                 },
-                sort: false,
-                onMove(){
+                sort: false,//elles doivent pas se trier elles memes au drag and drop
+                onMove() {//quant il y a déplacement
+                    resetTooltips()
                     instance.isEdited.set(true)
                 },
                 animation: 150,  // ms, animation speed moving items when sorting, `0` — without animation
             });
-
+            //on recupere le conteneur des catégories choisies
             let chosenCategories = document.getElementById("chosenCategories")
+            //et on lance sur lui aussi la propriété de drag and drop
             this.chosen = Sortable.create(chosenCategories, {
                 group: {
                     name: "chosenCategories",  // or { name: "...",
                     put: ['unchosenCategories']
                 },
-                onMove(){
+                onMove() {//quaant il y a déplacement
+                    resetTooltips()
                     instance.isEdited.set(true)
                 },
                 sort: true,  // sorting inside list
@@ -57,29 +71,39 @@ Template.editCategories.events({
             });
         }, 300)
     },
-    'click [saveCategoriesBtn]': function (event, instance) {
-        let methodToCall = instance.isProject ? "updateInfoItem" : "updateProfileItem";
-        let categories = []
-        $("#chosenCategories li").each((i,el)=>{
-            let id = el.getAttribute('id')
-            let category = parseInt(id.split('-')[1])
-            categories.push(category)
-        })
-        console.log(categories)
-        instance.owner.callMethod(
-            methodToCall,
-            "categories",
-            categories,
-            (error) => {
-                //si ca marche pas, on renvoie l'erreur par toast
-                if (error) {
-                    console.log(error)
-                    Materialize.toast("une erreur s'est produite", 4000, 'red')
-                } else {
-                    let message = instance.isProject ? "Les types d'activités du projet " : "Vos centres d'interets"
-                    Materialize.toast(message + "ont été mise à jour", 6000, 'green')
-                }
-            })
+    //lorsqu'on clique sur sauvegarder todo : ca marche pas que la sauvegarde se fasse au focus out
+    'click [saveCategoriesBtn], focusout #editCategories': function (event, instance) {
+        //si le template a bien été édité
+        if (instance.isEdited.get() === true) {
+            //on récupere la bonne méthode
+            let methodToCall = instance.isProject ? "updateInfoItem" : "updateProfileItem";
+            //on récupere les catégories directement dans le html
+            let categories = [];
+            $("#chosenCategories li").each((i, el) => {//on les récupere toute et pour chacunes
+                let id = el.getAttribute('id');//on prends leur id
+                let category = parseInt(id.split('-')[1]);//on garde que la fin
+                categories.push(category);//on les rentre dans le tableau des catégories a ajouter
+            });
+            //puis on appelle la méthode
+            instance.owner.callMethod(
+                methodToCall,
+                "categories",
+                categories,
+                (error) => {
+                    //si ca marche pas, on renvoie l'erreur par toast
+                    if (error) {
+                        console.log(error)
+                        Materialize.toast("une erreur s'est produite", 4000, 'red')
+                    } else {
+                        //si tout est bon, on avertit l'user et on referme le panneau d'édition
+                        let message = instance.isProject ? "Les types d'activités du projet " : "Vos centres d'interets"
+                        Materialize.toast(message + "ont été mise à jour", 6000, 'green')
+                        instance.isEditing.set(false)
+                        instance.isEdited.set(false)
+                    }
+                })
+        }
+
     }
 });
 
