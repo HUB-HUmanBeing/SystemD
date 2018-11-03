e6836f9232908a634f4e2c79854650d07ba08eb5
banquo <caramaschi@hotmail.com>
2018-11-03 09:26:04 +0100

    début de la page vitrine

---
+++ client/templates/showcase/showcaseAvantages/showcaseAvantages.js
@@ -0,0 +1,20 @@
+Template.showcaseAvantages.helpers({
+    //add you helpers here
+});
+
+Template.showcaseAvantages.events({
+    //add your events here
+});
+
+Template.showcaseAvantages.onCreated(function () {
+    //add your statement here
+});
+
+Template.showcaseAvantages.onRendered(function () {
+    //add your statement here
+});
+
+Template.showcaseAvantages.onDestroyed(function () {
+    //add your statement here
+});
+
---
+++ client/templates/showcase/showcaseTitle/showcaseTitle.js
@@ -0,0 +1,38 @@
+Template.showcaseTitle.helpers({
+    aimsArray: function () {
+        return [
+            {
+                imgUrl: "images/icon/user_icon.png",
+                title: "Un espace de rencontre",
+                text: ""
+            },
+            {
+                imgUrl: "images/icon/project_icon.png",
+                title: "Des outils collaboratif",
+                text: ""
+            },
+            {
+                imgUrl: "images/icon/sharing_icon.png",
+                title: "Un réseau de partage et de gratuité",
+                text: ""
+            },
+
+        ]
+    }
+});
+
+Template.showcaseTitle.events({
+    //add your events here
+});
+
+Template.showcaseTitle.onCreated(function () {
+    //add your statement here
+});
+
+Template.showcaseTitle.onRendered(function () {
+});
+
+Template.showcaseTitle.onDestroyed(function () {
+    //add your statement here
+});
+
