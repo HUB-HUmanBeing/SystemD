1a1ddf6392f1234aecdf32c65c014ab19ed9e2e5
banquo <rbanquo@protonmail.com>
2019-02-19 12:08:49 +0100

    language débuggé !!!

--- client/main.js
+++ client/main.js
@@ -1,36 +1,38 @@
 // Client entry point, imports all client code
+import {Meteor} from "meteor/meteor";
+
 Session.set('userAvatars', {})
 
 // test of translation
 import i18n from 'meteor/universe:i18n';
-
+Meteor.startup(() => {
 // somewhere in the page layout (or possibly in the router?)
-function getLang () {
-    return (
-        navigator.languages && navigator.languages[0] ||
-        navigator.language ||
-        navigator.browserLanguage ||
-        navigator.userLanguage ||
-        'fr-FR'
-    );
-}
+    function getLang() {
+        return (
+            navigator.languages && navigator.languages[0] ||
+            navigator.language ||
+            navigator.browserLanguage ||
+            navigator.userLanguage ||
+            'fr-FR'
+        );
+    }
 
-i18n.setLocale(getLang()).then(() => {
-    console.log(i18n.getLocale());
-}).catch((err) => {
-    console.log(err);
-});
+    i18n.setLocale(getLang()).then(() => {
+        console.log("i18n.isLoaded('fr')", i18n.isLoaded('fr-FR'));
+        console.log("i18n.isLoaded('en')", i18n.isLoaded('en-US'));
+    }).catch((err) => {
+        console.log(err);
+    });
 
 
 // Set namespace of templates
 
-if (i18n.getLocale() === 'fr-FR'){
-    console.log('lang ok');
-    Template.landingHead.bindI18nNamespace('landingHeadFr');
+    if (i18n.getLocale() === 'fr-FR') {
+        console.log('lang ok');
+        Template.landingHead.bindI18nNamespace('landingHead');
 
-}else{
-    console.log('encore US');
-    Template.landingHead.bindI18nNamespace('landingHeadEn');
-
-    
-}
+    } else {
+        console.log('encore US');
+        Template.landingHead.bindI18nNamespace('landingHead');
+    }
+})
