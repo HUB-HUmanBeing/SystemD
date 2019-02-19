01947648c786254c2d85517634b2e318efabdaf5
jul <refloatdev@protonmail.com>
2019-02-19 17:17:04 +0100

    test tapI18n

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@
 Session.set('userAvatars', {})
 
 // test of translation
-import i18n from 'meteor/universe:i18n';
+
 
 // somewhere in the page layout (or possibly in the router?)
 function getLang () {
@@ -14,23 +14,4 @@ function getLang () {
         'fr-FR'
     );
 }
-
-i18n.setLocale(getLang()).then(() => {
-    console.log(i18n.getLocale());
-}).catch((err) => {
-    console.log(err);
-});
-
-
-// Set namespace of templates
-
-if (i18n.getLocale() === 'fr-FR'){
-    console.log('lang ok');
-    Template.landingHead.bindI18nNamespace('landingHeadFr');
-
-}else{
-    console.log('encore US');
-    Template.landingHead.bindI18nNamespace('landingHeadEn');
-
-    
-}
+console.log(getLang());
