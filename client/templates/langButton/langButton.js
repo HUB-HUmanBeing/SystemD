f2d7ed772379d8569a7b62ca972fdecaea74a874
jul <refloatdev@protonmail.com>
2019-03-03 18:33:59 +0100

    update langbutton

--- client/templates/langButton/langButton.js
+++ client/templates/langButton/langButton.js
@@ -7,17 +7,17 @@ Template.langButton.helpers({
 
 Template.langButton.events({
     // add your events here
-    'change select':(function(event) {
-        console.log('thats changed', $("select option:selected").val()),
-        
-        localStorage.setItem('lang', $("select option:selected").val()),
 
-        myLang = localStorage.getItem('lang');
-        
-        i18n.setLocale(myLang),
-        console.log("getLocale: ", i18n.getLocale());
+    // when we change the language button 
+    'change select':(function(event) {
+        // we store the value of new language, for example: fr-FR, in localStorage
+        localStorage.setItem('lang', $("select option:selected").val());
+        // and we set the new language in i18n retrieving the value in localStorage(newLang)
+        let newLang = localStorage.getItem('lang');
+        i18n.setLocale(newLang);
         
       })
+
 });
 
 Template.langButton.onCreated(() => {
@@ -27,10 +27,12 @@ Template.langButton.onCreated(() => {
 
 Template.langButton.onRendered(() => {
     // add your statement here
-    let actualLang = i18n.getLocale().split('-')[0];
+
+    // we store the language in variable
+    let actualLang = localStorage.getItem('lang');
+    // we define the selected language(actualLang) in the language button
     $("select [value=" + actualLang + "]").prop("selected", true);
-    
-    console.log(i18n.getLocale());
+
 });
 
 Template.langButton.onDestroyed(() => {
