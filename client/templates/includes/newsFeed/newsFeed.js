94c6a93601b7b75d74bbfb77cf0e3463b64b33e5
robinBanquo <caramaschi@hotmail.com>
2019-11-20 21:40:22 +0100

    wip

--- client/templates/includes/newsFeed/newsFeed.js
+++ client/templates/includes/newsFeed/newsFeed.js
@@ -6,17 +6,16 @@ Template.newsFeed.helpers({
     isOpen: function () {
         return Template.instance().isOpen.get()
     },
-    isUpToDate: function() {
-        return false// Template.instance().isUpToDate.get()
+    isUpToDate: function () {
+        return false // Template.instance().isUpToDate.get()
     },
-    getContent: function () {
-        console.log(Template.instance())
-        return Template.instance().content.get()
+    newsArray: function () {
+        return Template.instance().newsArray.get()
     }
 });
 
 Template.newsFeed.events({
-    'click [openModal]' : function (event, instance) {
+    'click [openModal]': function (event, instance) {
         event.preventDefault();
         instance.isOpen.set(true);
         instance.isUpToDate.set(true);
@@ -29,36 +28,41 @@ Template.newsFeed.onCreated(function () {
     this.isOpen = new ReactiveVar(false);
     this.isUpToDate = new ReactiveVar(true);
     this.lastView = window.localStorage.getItem('newsFeedHist');
-    this.content = new ReactiveVar('');
+    this.newsArray = new ReactiveVar([]);
+
+    let url = Meteor.isDevelopment ? "news/newsFeed.json" :Meteor.settings.public.newsURL
+    console.log(url)
+    $.get(
+        url,
+        'false',
+        (newsJson) => {
+            //TODO marche pas à cause de la mise en cache de FF
+           // console.log(this.newsJson['lastUpdate'], this.lastView, Date.now());
+            if (newsJson['lastUpdate'] < this.lastView) {
+                this.isUpToDate.set(false);
+            }
+            this.newsArray.set(newsJson['newsArray'])
+            console.log(this.newsArray.get())
+        },
+        'json'
+    );
 });
 
 Template.newsFeed.onRendered(function () {
     //GET request for newsFeed
-    var template = this;
 
-    $.get(
-      'news/newsFeed.json',
-      'false',
-      function (newsJson) {
-        template.newsJson = newsJson;
-        //TODO marche pas à cause de la mise en cache de FF
-        console.log(template.newsJson['lastUpdate'],template.lastView,Date.now());
-        if(newsJson['lastUpdate']<template.lastView){
-          template.isUpToDate.set(false);
-        }
-        template.content=newsJson['new1']
-        console.log(template.content)
-      },
-      'json'
-    );
 
-    //Define modal 
+    //Define modal
     $('#modalNewsFeed').modal({
-      startingTop: '4%', // Starting top style attribute
-      endingTop: '4%',
-      complete: function() { 
-        template.isOpen.set(false);
-      } 
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '4%',
+        complete: () => {
+            this.isOpen.set(false);
+
+        },
+        ready: function (modal, trigger) {
+
+        }
     });
 });
 
