a69728528b97869a03f91d9d7a1047188b4b4390
banquo <caramaschi@hotmail.com>
2019-12-22 19:29:22 +0100

    mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -1,5 +1,16 @@
 App.info({
     id: 'com.meteor.system-d',
     name: 'System-D',
-    version: "0.0.2"
+    version: "0.0.2",
+    description: 'Encrypted platform for collaborative and free projects',
+    author: 'System-d team',
+    email: 'rbanquo@protonmail.com',
+    website: 'https://www.system-d.org/'
 });
+
+App.icons({
+    'iphone_2x': 'icons/icon-60@2x.png',
+    'iphone_3x': 'icons/icon-60@3x.png',
+    // More screen sizes and platforms...
+});
+App.setPreference('BackgroundColor', '0xff0000ff');
