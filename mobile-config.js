76bcf21e4359a45d08196393f761f87dfea007b8
robin <caramaschi@hotmail.com>
2020-11-17 10:20:38 +0100

    pret pour v2

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 App.info({
     id: 'com.systemd.systemd',
     name: 'System-D',
-    version: "0.0.7",
+    version: "0.0.8",
     description: 'Encrypted platform for collaborative and free projects',
     author: 'System-d team',
     email: 'rbanquo@protonmail.com',
@@ -31,33 +31,44 @@ App.setPreference('android-targetSdkVersion', '29', "android");
 
 
 // App.accessRule('http://*');
-App.accessRule('https://*',  { type: "intent" });
+App.accessRule('https://*', {type: "intent"});
 //App.accessRule('http://*');
 App.accessRule('https://www.system-d.org/*');
 App.accessRule('https://www.asset.system-d.org/*');
-App.accessRule('blob:*', { type: "intent" });
-App.accessRule('blob:*', { type: "navigation" });
-App.accessRule('data:*', { type: "intent" });
-App.accessRule('data:*', { type: "navigation" });
-App.accessRule('geo:*', { type: "intent" });
+App.accessRule('blob:*', {type: "intent"});
+App.accessRule('blob:*', {type: "navigation"});
+App.accessRule('data:*', {type: "intent"});
+App.accessRule('data:*', {type: "navigation"});
+App.accessRule('geo:*', {type: "intent"});
 //App.accessRule('*', { type: "intent" });
 
 App.accessRule('http://localhost:*');
-App.accessRule('https://raw.githubusercontent.com/HUB-HUmanBeing/SystemD/master/public/news/newsFeed.json', { type: 'navigation' });
+App.accessRule('https://raw.githubusercontent.com/HUB-HUmanBeing/SystemD/master/public/news/newsFeed.json', {type: 'navigation'});
 // App.accessRule('https://*', { type: 'navigation' });
-App.accessRule('*.openstreetmap.org/*', { type: 'navigation' });
-App.accessRule('*.tile.openstreetmap.com/*', { type: 'navigation' });
-App.accessRule('*.tile.openstreetmap.com/*', { type: 'navigation' });
-App.accessRule('https://server.arcgisonline.com/*', { type: 'navigation' });
-App.accessRule('http://a.tiles.mapbox.com/*', { type: 'navigation' });
-App.accessRule('https://liberapay.com/robinBanquo/donate', { type: 'navigation' });
-App.accessRule('https://liberapay.com/robinBanquo/donate', { type: 'intent' });
-App.accessRule('https://api.tiles.mapbox.com/*', { type: 'navigation' });
-App.accessRule('https://placeholdit.imgix.net/*', { type: 'navigation' });
-App.accessRule('https://framapiaf.org/*', { type: 'intent' });
-App.accessRule('https://www.facebook.com/*', { type: 'intent' });
-App.accessRule('https://twitter.com/*', { type: 'intent' });
-App.accessRule('https://riot.im/*', { type: 'intent' });
-App.accessRule('https://github.com/*', { type: 'intent' });
+App.accessRule('*.openstreetmap.org/*', {type: 'navigation'});
+App.accessRule('*.tile.openstreetmap.com/*', {type: 'navigation'});
+App.accessRule('*.tile.openstreetmap.com/*', {type: 'navigation'});
+App.accessRule('https://server.arcgisonline.com/*', {type: 'navigation'});
+App.accessRule('http://a.tiles.mapbox.com/*', {type: 'navigation'});
+App.accessRule('https://liberapay.com/robinBanquo/donate', {type: 'navigation'});
+App.accessRule('https://liberapay.com/robinBanquo/donate', {type: 'intent'});
+App.accessRule('https://api.tiles.mapbox.com/*', {type: 'navigation'});
+App.accessRule('https://placeholdit.imgix.net/*', {type: 'navigation'});
+App.accessRule('https://framapiaf.org/*', {type: 'intent'});
+App.accessRule('https://www.facebook.com/*', {type: 'intent'});
+App.accessRule('https://twitter.com/*', {type: 'intent'});
+App.accessRule('https://riot.im/*', {type: 'intent'});
+App.accessRule('https://github.com/*', {type: 'intent'});
 
 App.addResourceFile("cordova-build-override/google-services.json", "app/google-services.json", "android")
+
+App.appendToConfig(`
+    <edit-config file="app/src/main/AndroidManifest.xml" mode="merge" target="/manifest/application" xmlns:android="http://schemas.android.com/apk/res/android">
+        <application android:usesCleartextTraffic="true">
+            <trust-anchors>            
+              <certificates src="system" />        
+            </trust-anchors> 
+        </application>
+    </edit-config>
+`);
+
