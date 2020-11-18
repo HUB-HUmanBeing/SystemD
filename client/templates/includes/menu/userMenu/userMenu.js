53df9c107327a3177dd079f042bd2284663561b1
robin <caramaschi@hotmail.com>
2020-11-18 09:32:34 +0100

    debut

---
+++ client/templates/includes/menu/userMenu/userMenu.js
@@ -0,0 +1,20 @@
+Template.userMenu.helpers({
+    //add you helpers here
+});
+
+Template.userMenu.events({
+    //add your events here
+});
+
+Template.userMenu.onCreated(function () {
+    //add your statement here
+});
+
+Template.userMenu.onRendered(function () {
+    //add your statement here
+});
+
+Template.userMenu.onDestroyed(function () {
+    //add your statement here
+});
+
